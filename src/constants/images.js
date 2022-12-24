cff9bdc05550573fd8108112378a5950ee0ef34e
Luca Luzzi <luzziluca6@gmail.com>
2022-12-24 12:54:17 -0300

    public cambiado

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,15 +1,15 @@
-import profilePic from '/amorchitoo.png';
-import gifApp from '/gifexpertapp.png';
-import gpt3 from '/gpt3.png';
-import hoobank from '/hoobank.png';
-import restaurante from '/restaurante.png';
-import reduxStore from '/reduxstore.png';
-import cssIcon from '/iconos_css.png';
-import htmlIcon from '/iconos_html.png';
-import jsIcon from '/iconos_js.png';
-import reactIcon from '/iconos_react-js.png';
-import nodeIcon from '/iconos_node-js.png';
-import tailwindIcon from '/iconos_tailwind-css.png';
+import profilePic from '../assets/amorchitoo.png';
+import gifApp from '../assets/gifexpertapp.png';
+import gpt3 from '../assets/gpt3.png';
+import hoobank from '../assets/hoobank.png';
+import restaurante from '../assets/restaurante.png';
+import reduxStore from '../assets/reduxstore.png';
+import cssIcon from '../assets/iconos_css.png';
+import htmlIcon from '../assets/iconos_html.png';
+import jsIcon from '../assets/iconos_js.png';
+import reactIcon from '../assets/iconos_react-js.png';
+import nodeIcon from '../assets/iconos_node-js.png';
+import tailwindIcon from '../assets/iconos_tailwind-css.png';
 
 
 export default {
